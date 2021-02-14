2843ac332c0b8b8f7bd9fe6d6ff65bd367c268c0
Leslie <leslie_d_n@hotmail.com>
2021-02-14 03:06:17 -0700

    i fix the day

--- Hackathon2021/src/application/Event.java
+++ Hackathon2021/src/application/Event.java
@@ -39,7 +39,7 @@ public class Event {
 	public Event(String name, String day[], int hr, int min, int duration, String url) {
 		setTime(hr,min);
 		setName(name);
-		setDay(day[]);
+		setDay(day);
 		setDuration(duration);
 		setUrl(url);
 	}

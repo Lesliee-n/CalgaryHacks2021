a0621fa702541ba05e2d7844cb5e94327804d464
Jeffery <jeffrey7553@gmail.com>
2021-02-14 01:50:06 -0700

    glorify list

--- Hackathon2021/src/application/Schedule.java
+++ Hackathon2021/src/application/Schedule.java
@@ -17,35 +17,9 @@ public class Schedule {
         return courses;
     }
 
-    public boolean checkEventTime(int time) {
-        for(int i = 0; i < this.events.size(); i++) {
-            // depends on if time is an int or a string.
-            if (this.events.get(i).getTime() == time) {
-                return true;
-            }
-        }
-        return false;
-    }
+   
 
-    public int getMinTime() {
-        int timeMin = Integer.MAX_VALUE;
-        for (int i = 0; i < this.events.size(); i++) {
-            if (this.events.get(i).getTime() < timeMin) {
-                timeMin = this.events.get(i).getTime();
-            }
-        }
-        return timeMin;
-    }
-
-    public int getMaxTime() {
-        int timeMax = 0;
-        for (int i = 0; i < this.events.size(); i++) {
-            if (this.events.get(i).getTime() > timeMax) {
-                timeMax = this.events.get(i).getTime();
-            }
-        }
-        return timeMax;
-    }
+  
 
 
     public void deleteEvent(Event e) {

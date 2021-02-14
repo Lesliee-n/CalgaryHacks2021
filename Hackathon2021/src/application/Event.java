3de2c028433052c10b0fec7032ee8a3491946bd7
Leslie <leslie_d_n@hotmail.com>
2021-02-13 20:01:06 -0700

    Basic Event setters and getters

--- Hackathon2021/src/application/Event.java
+++ Hackathon2021/src/application/Event.java
@@ -1,5 +1,109 @@
 package application;
+import java.time.LocalDate;
+import java.util.Arrays;
 
+//Event object
 public class Event {
+	private int time;
+	private String day[];
+	private String name;
+	private int duration;
+	private String url;
+	private String password;
+	private boolean eventActive;
+	
+	
+}
 
+
+
+//set variables in object
+public void setTime(int newTime) {
+	this.time = newTime;
+	
+}
+
+public void setDay(String newDay[]) {
+	
+	for(int i : newDay) {
+		day[i]=newDay[i];
+	}
+	
+}
+
+public void setName(String newName) {
+	this.name = newName;
+	
+}
+
+public void setDuration(String newDuration) {
+	this.duration = newDuration;
+	
+}
+
+public void setUrl(String newUrl) {
+	this.url = newUrl;
+	
+}
+
+public void setPassword(String newPassword) {
+	this.password = newPassword;
+	
+}
+
+public void eventActive(boolean newEventActive) {
+	this.eventActive = newEventActive;
+	
+}
+
+
+
+
+
+//get variables in object
+public int getTime() {
+	return time;
+	
+}
+
+public String[] getDay() {
+	return day;
+	
+}
+
+public String getName() {
+	return name;
+	
+}
+
+public String getDuration() {
+	return duration;
+	
+}
+
+public String getUrl() {
+	return url;
+	
+}
+
+public String getPassword() {
+	return password;
+	
+}
+
+public boolean eventActive() {
+	return eventActive;
+	
+}
+
+
+
+
+
+public void launchMeeting() {
+	system.out.print("launch meeting");
+}
+
+public void toggleEventActive() {
+	system.out.print("toggle event active");
 }

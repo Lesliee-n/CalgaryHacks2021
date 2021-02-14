9f4f32cabc6a9bf32556d1aaa7f97824e9fe9b62
Jeffery <jeffrey7553@gmail.com>
2021-02-13 18:55:28 -0700

    Main class files

---
+++ Hackathon2021/src/application/MainScreen.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class MainScreen extends Scene{
+
+	public MainScreen() {
+		super(new BorderPane(),800,600);
+		// TODO Auto-generated constructor stub
+	}
+
+}
